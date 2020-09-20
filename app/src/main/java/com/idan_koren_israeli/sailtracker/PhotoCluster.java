7994531ec20785f990e682c4be973004f7852483
Idan Koren-Israeli <39092077+Idan503@users.noreply.github.com>
2020-09-20 18:06:09 +0300

    Added basic layout design to all activities

---
+++ app/src/main/java/com/idan_koren_israeli/sailtracker/PhotoCluster.java
@@ -0,0 +1,66 @@
+package com.idan_koren_israeli.sailtracker;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ *
+ * This fragment is used to be like Instagram feed
+ * Each user has an associated photos from sails
+ *
+ *
+ */
+public class PhotoCluster extends Fragment {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public PhotoCluster() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment PhotoCluster.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static PhotoCluster newInstance(String param1, String param2) {
+        PhotoCluster fragment = new PhotoCluster();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_photo_cluster, container, false);
+    }
+}
